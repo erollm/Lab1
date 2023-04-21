e3762f908824b12636efe146b6d4a2f7c43a4721
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-04-21 23:29:42 +0200

    removed duplication bug

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../assets/css/SearchPage.css";
+import { useLocation } from "react-router-dom";
+import axios from "../axios.js";
 
 function SearchPage() {
+  //Getting query parameters
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const genre = queryParams.get("genre");
+
+  const [search, setSearch] = useState("");
+
+  function handleChange(e) {
+    e.preventDefault();
+    setSearch(e.target.value);
+  }
+
   return (
     <div className="search_page">
-      <p>hello</p>
+      <input
+        type="text"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={search}
+      />
+      <h2 className="search_page_title">Results for : {genre}</h2>
     </div>
   );
 }
