5394c11c4e173028438e2b4b89b213fecd58f18e
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-04-22 08:02:21 +0200

    Search page design done.

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -2,30 +2,91 @@ import React, { useState, useEffect } from "react";
 import "../assets/css/SearchPage.css";
 import { useLocation } from "react-router-dom";
 import axios from "../axios.js";
+import request from "../request";
 
 function SearchPage() {
+  // state and effect declaration
+  const [search, setSearch] = useState("");
+  const [movie, setMovie] = useState([]);
+
   //Getting query parameters
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const genre = queryParams.get("genre");
 
-  const [search, setSearch] = useState("");
+  // url of photos in API
+  const base_url = "https://image.tmdb.org/t/p/original";
 
+  // Api call for later use
+  async function fetchData(fetchUrl) {
+    const response = await axios.get(fetchUrl);
+    setMovie(response.data.results);
+    return;
+  }
+
+  //Using map to delcare key-value pair
+  const genreMap = new Map([
+    ["trending", request.fetchTrending],
+    ["Trending", request.fetchTrending],
+    ["comedy", request.fetchComedyMovies],
+    ["Comedy", request.fetchComedyMovies],
+    ["Action", request.fetchActionMovies],
+    ["action", request.fetchActionMovies],
+    ["horror", request.fetchHorrorMovies],
+    ["Horror", request.fetchHorrorMovies],
+    ["documentaries", request.fetchDocumentaries],
+    ["Documentaries", request.fetchDocumentaries],
+  ]);
+
+  // conditional rendering
+  useEffect(() => {
+    const searchTerm = (genre || search || "").toLowerCase();
+    const fetchUrl = genreMap.get(searchTerm);
+
+    if (fetchUrl) {
+      fetchData(fetchUrl);
+    }
+  }, [search]);
+
+  // Fully functional search input and saving value onchange
   function handleChange(e) {
     e.preventDefault();
     setSearch(e.target.value);
   }
 
   return (
-    <div className="search_page">
+    <>
+      <p style={{ color: "white" }}>THIS IS A TEMPORARY INPUT</p>
       <input
         type="text"
         placeholder="Search here"
         onChange={handleChange}
         value={search}
       />
-      <h2 className="search_page_title">Results for : {genre}</h2>
-    </div>
+
+      <div className="search_page">
+        {search ? (
+          <h2 className="search_page_title">
+            Results for : {search ? search : genre}
+          </h2>
+        ) : (
+          <h2 className="search_page_title">Search a movie</h2>
+        )}
+
+        <div className="search_page_movies">
+          <div className="row_posters_search_container">
+            {movie.map((movie) => (
+              <img
+                className={`row_posters_search_item row_poster_large`}
+                key={movie.id}
+                src={`${base_url}${movie.poster_path}`}
+                alt={movie.name}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 export default SearchPage;
