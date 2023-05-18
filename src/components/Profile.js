d4ef45acb87800dfe755b5f93f3287faed4eb250
Zgjim Grajçevci <grajqevcizgjim03@icloud.com>
2023-05-18 19:14:51 +0200

    Added tabs in profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,8 +7,17 @@ import spiritedAway from '../assets/images/covers/spiritedaway.jpg';
 import totoro from '../assets/images/covers/totoro.jpg';
 import weatheringWithYou from '../assets/images/covers/weatheringwithyou.jpg';
 import yourName from '../assets/images/covers/yourname.jpg';
+// import {useState} from "@types/react";
+import { useState } from "react";
+
 
 const Profile = () =>{
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+
     return(
         <div className="profile container ">
             <div className="profileContainer row">
@@ -22,18 +31,30 @@ const Profile = () =>{
                         <a href="#">Edit</a>
                     </div>
                 </div>
-                <div className="profileWatched col-lg-8">
-                    <h1>Watched</h1>
-                    <div className="profileWatchedMovies">
-                        <img src={demonSlayer} alt=""/>
-                        <img src={dragonBall} alt=""/>
-                        <img src={naruto} alt=""/>
-                        <img src={onePiece} alt=""/>
-                        <img src={silentVoice} alt=""/>
-                        <img src={spiritedAway} alt=""/>
-                        <img src={totoro} alt=""/>
-                        <img src={weatheringWithYou} alt=""/>
-                        <img src={yourName} alt=""/>
+                <div className="profileActivities col-lg-8">
+                    <div className="bloc-tabs">
+                        <button className={toggleState === 1 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(1)}>Watched</button>
+                        <button className={toggleState === 2 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(2)}>Continue Watching</button>
+                    </div>
+                    <div className="content-tabs">
+                        <div className={toggleState === 1 ? "content  active-content" : "content"}>
+                            <div className="profileWatchedMovies">
+                                <img src={dragonBall} alt=""/>
+                                <img src={naruto} alt=""/>
+                                <img src={onePiece} alt=""/>
+                                <img src={silentVoice} alt=""/>
+                                <img src={totoro} alt=""/>
+                                <img src={weatheringWithYou} alt=""/>
+                                <img src={yourName} alt=""/>
+                            </div>
+                        </div>
+                        <div className={toggleState === 2 ? "content  active-content" : "content"}>
+                            <div className="profileWatchedContinue">
+                                <img src={demonSlayer} alt=""/>
+                                <img src={spiritedAway} alt=""/>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
