a4f316fc3c8b237872d9a66fda289d72fe81e27b
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-05-18 19:28:10 +0200

    Admin dashboard grid completed

--- src/admin/index.jsx
+++ src/admin/index.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Navbar from "./navbar";
-import "../assets/css/navbar.css";
+import "../assets/css/admin.css";
 
 function index() {
   return (
     <div className="admin--panel">
       <Navbar />
+      <div className="admin--main">
+        <div className="admin--main--header">
+          <h2>Admin Dashboard</h2>
+        </div>
+        <div className="admin--main--stats">
+          <p>MOVIES</p>
+          <p>USERS</p>
+          <p>MOVIES WATCHED</p>
+          <p>MOVIE GENRES</p>
+        </div>
+        <hr></hr>
+        <div className="admin--main--charts">
+          <p>Users logins in days</p>
+          <p>Movie watched in days</p>
+        </div>
+      </div>
     </div>
   );
 }
