75616bfc0396bc6fd714fe76d89735d8aa604ed6
AlbanSahitii <109045631+AlbanSahitii@users.noreply.github.com>
2023-06-06 05:36:14 +0200

    Search page done.

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import axios from "../axios.js";
+
 import demonSlayer from "../assets/images/covers/demonslayer.webp";
 import dragonBall from "../assets/images/covers/dragonballbroly.jpg";
 import naruto from "../assets/images/covers/naruto.webp";
@@ -9,25 +12,42 @@ import weatheringWithYou from "../assets/images/covers/weatheringwithyou.jpg";
 import yourName from "../assets/images/covers/yourname.jpg";
 
 function SearchPage() {
+  const queryParams = new URLSearchParams(window.location.search);
+  const query = queryParams.get("query");
+  const base_url = "https://image.tmdb.org/t/p/original";
+
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(`api/search/${query}`);
+      setMovie(request.data);
+      return;
+    }
+    fetchData();
+  });
+
+  console.log(movie);
+
   return (
-      <div className="searchPage container">
-        <div className="searchPageContainer">
-          <div className="searchPageTitle">
-            <h2>Results for:</h2>
-          </div>
-          <div className="searchPageResults">
-            <img src={demonSlayer} alt=""/>
-            <img src={dragonBall} alt=""/>
-            <img src={naruto} alt=""/>
-            <img src={onePiece} alt=""/>
-            <img src={silentVoice} alt=""/>
-            <img src={spiritedAway} alt=""/>
-            <img src={totoro} alt=""/>
-            <img src={weatheringWithYou} alt=""/>
-            <img src={yourName} alt=""/>
-          </div>
+    <div className="searchPage container">
+      <div className="searchPageContainer">
+        <div className="searchPageTitle">
+          <h2>{query ? `Results for : ${query} ` : null}</h2>
+        </div>
+        <div className="searchPageResults">
+          {movie.map((movie) => (
+            <a href={`/MoviePage?movie_id=${movie.id}`}>
+              <img
+                key={movie.id}
+                src={`${base_url}${movie.poster_path}`}
+                alt={movie.name}
+              />
+            </a>
+          ))}
         </div>
       </div>
+    </div>
   );
 }
 export default SearchPage;
