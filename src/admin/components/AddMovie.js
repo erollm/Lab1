8e83766a18caa7afff0067d785f6e86746534295
erollm <erollmuqkurtaj@gmail.com>
2023-06-15 04:28:25 +0200

    Add Movie Implemented

--- src/admin/components/AddMovie.js
+++ src/admin/components/AddMovie.js
@@ -5,62 +5,25 @@ import MovieContext from "../../context/MovieContext";
 
 
 export default function AddMovie() {
-    const { movieApi, getMovieApi } = useContext(MovieContext);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      const fetchMovies = async () => {
-        await getMovieApi(11);
-        setLoading(false);
-      };
-  
-      fetchMovies();
-    }, []);
-  
-    if (loading) {
-      return <p>Loading movies...</p>;
+    const {formValues, storeMovie, getMovieApi } = useContext(MovieContext);
+    const [movieId, setMovieId] = React.useState("");
+
+    function vale(){
+       getMovieApi(movieId);
     }
-    console.log(movieApi)
     return (
         <div className="adminContainer">
             <div className="SContainer">
                 <h2>Add New Movie</h2>
-                <div className="AddUser">
+                <form className="AddUser" onSubmit={storeMovie}>
                     <label>Title</label>
-                    <input type="text"></input>
+                    <input disabled type="text" value={formValues.title}></input>
                     <label>Description</label>
-                    <textarea rows="5"></textarea>
-                    <label>Trailer Url</label>
-                    <input type="text"></input>
-                    <label>IMDB</label>
-                    <input type="text"></input>
-                    <label>Genre</label>
-                    <select>
-                        <option value="Action">Action</option>
-                        <option value="Adventure">Adventure</option>
-                        <option value="Animation">Animation</option>
-                        <option value="Comedy">Comedy</option>
-                        <option value="Crime">Crime</option>
-                        <option value="Drama">Drama</option>
-                        <option value="Fantasy">Fantasy</option>
-                        <option value="Horror">Horror</option>
-                        <option value="Mystery">Mystery</option>
-                        <option value="Romance">Romance</option>
-                        <option value="Sci-Fi">Sci-Fi</option>
-                        <option value="Thriller">Thriller</option>
-                    </select>
-                    <span>
-                    <label htmlFor="file-upload" className="custom-file-upload">
-                    <FontAwesomeIcon icon={faArrowUpFromBracket} /> Upload Movie
-                    </label>
-      <input id="file-upload" type="file" />
-                    <label htmlFor="file-upload" className="custom-file-upload">
-                    <FontAwesomeIcon icon={faArrowUpFromBracket} /> Upload Cover
-                    </label>
-                    <input id="file-upload" type="file" />
-                    </span>
+                    <textarea disabled rows="5" value={formValues.description}></textarea>
+                    <label>TheMovieDB ID:</label>
+                    <input onBlur={vale} onChange={(e)=> setMovieId(e.target.value)}type="text"></input>
                     <button className="btn btn-primary addB">Add</button>
-                </div>
+                </form>
             </div>
         </div>
     )
--- src/context/MovieContext.js
+++ src/context/MovieContext.js
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "../api/axios";
+import axios from "../axios.js";
 
 const MovieContext = createContext();
 
@@ -9,9 +9,21 @@ export const MovieProvider = ({children}) => {
   const movieId = queryParams.get("movie_id");
   const base_url = "https://image.tmdb.org/t/p/original";
   const apiKEY= "2fad590bc404e4e882a683070f03d08b";
+  const navigate = useNavigate();
 
+  const initialValues = {
+    title: "",
+    backdrop_path:"",
+    trending: true,
+    poster_path: "",
+    length: "",
+    date: "",
+    description: "",
+    trailer: ""
+  }
+  const [formValues, setFormValues] = useState(initialValues);
   const [movies, setMovies] = useState(null);
-  const [movieApi, setMovieApi] = useState();
+
 
   const getMovies = async() => {
     const request = await axios.get(`api/v1/movies/`);
@@ -24,11 +36,31 @@ export const MovieProvider = ({children}) => {
   }
   
   const getMovieApi = async (id) =>{
-    const movieGet = await axios.get(`https://api.themoviedb.org/3/movie/11??api_key=${apiKEY}`);
-    setMovieApi(movieGet);
+    const movieGet = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKEY}`);
+    setFormValues({
+      title: movieGet.data.title,
+      backdrop_path: movieGet.data.backdrop_path,
+      trending: true,
+      poster_path: movieGet.data.poster_path,
+      length: movieGet.data.runtime,
+      date: movieGet.data.release_date,
+      description: movieGet.data.overview,
+      trailer: ""
+  })
+}
+
+const storeMovie = async (e) =>{
+  e.preventDefault();
+  try{
+      await axios.post("/api/v1/movies", formValues);
+      setFormValues(initialValues);
+      navigate("/admin/movies");
+}
+  catch(e){
+      console.log(e);
   }
-   
-  return <MovieContext.Provider value={{movies, getMovies, deleteMovie, movieApi, getMovieApi}}>{children}</MovieContext.Provider>
+}
+  return <MovieContext.Provider value={{movies, getMovies, deleteMovie, storeMovie, getMovieApi, formValues}}>{children}</MovieContext.Provider>
 };
 
 export default MovieContext;
